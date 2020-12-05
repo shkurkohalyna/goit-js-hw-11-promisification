522db20032b375be0bd8b38ae3ae1b3312b2c65c
shkurkohalyna <krysty1105@gmail.com>
2020-12-05 23:30:33 +0200

    ftgdgh

---
+++ src/js/task01.js
@@ -0,0 +1,17 @@
+export const delay = ms => {
+  
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    const time = ms
+    resolve(time);
+  }, ms);
+  });  
+  return promise;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
---
+++ src/js/task02.js
@@ -0,0 +1,22 @@
+export const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+export const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map
+      (user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      );
+    resolve(updatedUsers);
+})
+  
+};
+
+export const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
